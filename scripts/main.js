5ea6ad401d8305b2067eef583b9aefc945bd9781
SUPTO-1 <jahidulislamsupta9@gmail.com>
2024-02-21 13:44:12 +0600

    showing ticket information

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
+document.getElementById("coupon").addEventListener("input", couponApplyPart);
 function ticketKataStarts(id)
 {
-    const button = document.getElementById(id);
     setBackgroundColor(id);
 }
 
@@ -18,6 +18,7 @@ function setBackgroundColor(id)
           button.style.backgroundColor = "#1DD100";
           increaseSelectedSeat();
           decreaseAvailableSeats();
+          setTicketInformation(id);
         }
         else
         {
@@ -62,4 +63,37 @@ function increaseAvailableSeats()
     let seatCountInt = parseInt(seatCount);
     seatCountInt+=1;
     selectedSeat.innerText = seatCountInt;
+}
+
+function couponApplyPart()
+{
+    const coupon = document.getElementById("coupon");
+    const couponValue = coupon.value;
+    if(couponValue == "NEW15")
+    {
+        const apply = document.getElementById("apply");
+        apply.removeAttribute("disabled");
+    }
+    else if(couponValue == "Couple 20")
+    {
+        const apply = document.getElementById("apply");
+        apply.removeAttribute("disabled");
+    }
+}
+function setTicketInformation(id)
+{
+    const seatName = id;
+    let newDiv1 = document.createElement("div");
+    newDiv1.setAttribute("class","flex justify-between mb-6");
+    let newH1 = document.createElement("h1");
+    newH1.setAttribute("class","text-[16px] inter-font font-medium");
+    let newP1 = document.createElement("p");
+    newP1.setAttribute("class","text-[16px] inter-font font-medium");
+    let newP2 = document.createElement("p");
+    newP2.setAttribute("class","text-[16px] inter-font font-medium");
+    newH1.innerText = seatName;
+    newP1.innerText = "Economy";
+    newP2.innerText = "550";
+    newDiv1.append(newH1,newP1,newP2);
+    document.getElementById("seat-information-container").append(newDiv1);
 }
